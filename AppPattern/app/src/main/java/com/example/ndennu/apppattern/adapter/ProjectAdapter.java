b17cfbfb7334d88f7b1e7f0fe652e9f7f57d7c77
nicolas dennu <dennu.nicolas@gmail.com>
2018-06-27 17:31:59 +0200

    lib

---
+++ AppPattern/app/src/main/java/com/example/ndennu/apppattern/adapter/ProjectAdapter.java
@@ -0,0 +1,43 @@
+package com.example.ndennu.apppattern.adapter;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.ndennu.todolib.model.Project;
+
+import java.util.List;
+
+
+public class ProjectAdapter extends RecyclerView.Adapter<ProjectAdapter.ViewHolder> {
+
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+}
---
+++ AppPattern/todolib/src/main/java/com/example/ndennu/todolib/model/Task.java
@@ -0,0 +1,31 @@
+package com.example.ndennu.todolib.model;
+
+public class Task {
+    private int id;
+    private String text;
+    private int id_project;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public int getId_project() {
+        return id_project;
+    }
+
+    public void setId_project(int id_project) {
+        this.id_project = id_project;
+    }
+}

2204a3445995a5e5e039165e864c245ad745a135
nicolas dennu <dennu.nicolas@gmail.com>
2018-06-27 18:29:30 +0200

    project view

--- AppPattern/app/src/main/java/com/example/ndennu/apppattern/MainActivity.java
+++ AppPattern/app/src/main/java/com/example/ndennu/apppattern/MainActivity.java
@@ -1,13 +1,44 @@
 package com.example.ndennu.apppattern;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+
+import com.example.ndennu.apppattern.adapter.ProjectAdapter;
+import com.example.ndennu.todolib.model.Project;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.recycler_project) RecyclerView recyclerProject;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        recyclerProject.setLayoutManager(new LinearLayoutManager(this));
+
+        List<Project> projects = new ArrayList<>();
+        projects.add(new Project());
+        projects.add(new Project());
+
+        ProjectAdapter projectAdapter = new ProjectAdapter(projects);
+
+        projectAdapter.setListener(new ProjectAdapter.Listener() {
+            @Override
+            public void onGenreClick(Project project) {
+                Log.i("azeyuiop", "qsdfhklm");
+            }
+        });
+        recyclerProject.setAdapter(projectAdapter);
     }
 }
--- AppPattern/app/src/main/java/com/example/ndennu/apppattern/adapter/ProjectAdapter.java
+++ AppPattern/app/src/main/java/com/example/ndennu/apppattern/adapter/ProjectAdapter.java
@@ -10,34 +10,67 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.example.ndennu.apppattern.R;
 import com.example.ndennu.todolib.model.Project;
 
 import java.util.List;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 
 public class ProjectAdapter extends RecyclerView.Adapter<ProjectAdapter.ViewHolder> {
 
+    private List<Project> projectList;
+    private Listener listener;
+
+    public ProjectAdapter(List<Project> projectList) {
+        this.projectList = projectList;
+    }
+
+    public void setListener(Listener listener) {
+        this.listener = listener;
+    }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View itemView = LayoutInflater
+                .from(parent.getContext())
+                .inflate(R.layout.item_project, parent, false);
+        return new ViewHolder(itemView);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.titleProject.setText("pmlokijuhytg");
 
+        final Project p = projectList.get(position);
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (listener != null) listener.onGenreClick(p);
+            }
+        });
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return projectList.size();
     }
 
     class ViewHolder extends RecyclerView.ViewHolder {
 
+        @BindView(R.id.title_project) TextView titleProject;
+
         public ViewHolder(View itemView) {
             super(itemView);
+            ButterKnife.bind(this, itemView);
         }
+    }
+
+    public interface Listener {
+        void onGenreClick(Project project);
     }
 }
